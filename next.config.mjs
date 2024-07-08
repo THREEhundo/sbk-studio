711bd741f60947d4ee6e950e580ba5e7342b3d19
threehund0 <samuelbaik87@gmail.com>
2024-07-08 12:30:43 -0700

    Created placeholder image component & implemented in page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+  	dangerouslyAllowSVG: true,
+    domains: ['placehold.co'],
+  },
+}
 
 export default nextConfig;
--- src/components/ui/Placeholder.js
+++ src/components/ui/Placeholder.js
@@ -1,12 +1,13 @@
-import Image from 'next/image'
 import React from 'react'
+import Image from 'next/image'
 
 const Placeholder = ({ width, height, type, className }) => {
 	const sizes = type === 'icon' ? '(max-width: 768px, 48px, 24px' : '100vw'
 	const alt = type === 'icon' ? 'Placeholder icon' : 'Placeholder image'
 	return (
 		<Image
-			src={`/api/placeholder/${width}/${height}`}
+			src={`https://placehold.co/${width}x${height}`}
+			//src={`https://via.placeholder.com/${width}/${height}`}
 			width={width}
 			height={height}
 			alt={alt}
