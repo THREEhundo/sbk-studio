e3bca2918de00cf19ebba6b53d1f6c215e20e124
threehund0 <samuelbaik87@gmail.com>
2024-07-28 18:00:18 -0700

    Turned Header into a client component for Blog

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,37 +1,63 @@
-import React from 'react'
-import BlogPostCard from '../ui/BlogPostCard'
+'use client'
+import React, { useEffect, useRef, useState } from 'react'
 import AboveTheFold from './AboveTheFold'
-import { getHeaderData } from '@/lib/pageData'
+import dynamic from 'next/dynamic'
+import BlogPostCard from '../ui/BlogPostCard'
 
-const Header = async ({ isBlogPage = false, heroData, featureArticle }) => {
-	try {
-		const headerData = await getHeaderData()
+const Header = ({ isBlogPage = false, heroData, featureArticle }) => {
+	const [cardContainerHeight, setCardContainerHeight] = useState('auto')
+	const [maxHeight, setMaxHeight] = useState('auto')
+	const headerRef = useRef(null)
+	const h1Ref = useRef(null)
 
-		if (!heroData && !featureArticle) {
-			throw new Error('Specific page data not found')
+	useEffect(() => {
+		const updateCardHeight = () => {
+			if (headerRef.current && h1Ref.current) {
+				const viewportHeight = window.innerHeight
+				const navHeight = 88 // Based on your calc(100vh-88px)
+				const h1Height = h1Ref.current.offsetHeight
+				const remainingHeaderHeight = viewportHeight - navHeight
+				const remainingImageHeight = remainingHeaderHeight - h1Height
+				setMaxHeight(`${remainingImageHeight - 181}px`)
+			}
 		}
+		updateCardHeight()
+		window.addEventListener('resize', updateCardHeight)
 
-		return (
-			<header className='responsive-container bg-neutral h-[calc(100vh-88px)]  pb-12'>
-				{isBlogPage ? (
-					<div>
-						<h1 className='text-3xl md:text-4xl lg:text-5xl font-bold pb-4 text-primary-500'>
-							Blog
-						</h1>
+		return () => window.removeEventListener('resize', updateCardHeight)
+	}, [maxHeight, cardContainerHeight])
 
+	const containerStyles = `h-[calc(100vh-88px)] pb-12`
+	const featureStyles = `max-h-[${maxHeight}] flex flex-col`
+	console.log('++', maxHeight)
+	return (
+		<header
+			ref={headerRef}
+			className={`responsive-container bg-neutral ${
+				!isBlogPage ? containerStyles : featureStyles
+			}`}>
+			{isBlogPage ? (
+				<>
+					<h1
+						ref={h1Ref}
+						className='text-3xl md:text-4xl lg:text-5xl font-bold pb-4 text-primary-500'>
+						Blog
+					</h1>
+					<div className=''>
 						{featureArticle && (
-							<BlogPostCard post={featureArticle} />
+							<BlogPostCard
+								post={featureArticle}
+								className='h-full'
+								maxHeight={maxHeight}
+							/>
 						)}
 					</div>
-				) : (
-					<AboveTheFold isHomepage={true} obj={heroData} />
-				)}
-			</header>
-		)
-	} catch (error) {
-		console.error('Error in Header component:', error)
-		return <header>Error loading header content</header>
-	}
+				</>
+			) : (
+				<AboveTheFold isHomepage={true} obj={heroData} />
+			)}
+		</header>
+	)
 }
 
 export default Header
